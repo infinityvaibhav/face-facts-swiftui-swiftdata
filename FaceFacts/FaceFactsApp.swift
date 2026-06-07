bdd73e069301e330bf745e430dbcd71c42d6bfe9
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2026-06-07 15:53:47 +0530

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by वैभव उपाध्याय on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceFactsTests/FaceFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsTests.swift
+//  FaceFactsTests
+//
+//  Created by वैभव उपाध्याय on 07/06/26.
+//
+
+import Testing
+@testable import FaceFacts
+
+struct FaceFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
